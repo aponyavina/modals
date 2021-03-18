8c585e4e514f6799b64a6e6202e7493fbc599f3a
aponyavina <ponyavina1997@mail.ru>
2021-03-18 15:17:01 +0300

    creating popup second (form)

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,36 @@
       <section>
         <div class="container">
 
+          <!-- first modal -->
           <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show First Modal</button>
-
           <Modals 
           title="First Modal" 
           v-show="modalFirst"
-          @close="modalFirst = !modalFirst"
+          @close="modalFirst = false"
           >
           <div slot="body">
             <p>Text</p>
             <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well done!</button>
+          </div>
+          </Modals>
+
+          <!-- second modal -->
+          <button class="btn btnPrimary" @click="modalSecond.show = !modalSecond.show">Show Second Modal</button>
+          <Modals 
+          title="Second Modal" 
+          v-show="modalSecond.show"
+          @close="modalSecond.show = false"
+          >
+          <div slot="body">
+            <!-- <p>Text</p> -->
+            <form @submit.prevent="submitSecondForm">
+            <label>Name:</label>
+            <input type="text" v-model="modalSecond.name">
+            <label>E-mail:</label>
+            <input type="email" v-model="modalSecond.email">
+            <button class="btn btnPrimary">Submit!</button>
+
+            </form>
           </div>
           </Modals>
 
@@ -34,7 +54,23 @@ export default {
   },
   data() {
     return {
-      modalFirst: false
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email
+      });
+      this.modalSecond.name = ''
+      this.modalSecond.email = ''
+      this.modalSecond.show = false
     }
   }
 }
